ee42a2c4c6acf2694c857474a085006133b2beb7
DFST <mk@dfst.io>
2025-03-20 22:40:19 +0300

    bigint

--- packages/ui/src/lib/dex/public-key.ts
+++ packages/ui/src/lib/dex/public-key.ts
@@ -9,12 +9,18 @@ const client = new Client({
 });
 
 export async function publicKeyToU256(publicKey: string): Promise<bigint> {
-  client.publicKeyToRaw;
+  const raw = client.publicKeyToRaw(publicKey);
+  const publicKeyBigint = BigInt("0x" + raw);
+  console.log("publicKeyBigint", publicKeyBigint);
   const publicKeyInternal: PublicKey =
     typeof publicKey === "string" ? PublicKey.fromBase58(publicKey) : publicKey;
   const x = publicKeyInternal.x.toBigInt();
   const isOdd = publicKeyInternal.isOdd.toBoolean();
-  return isOdd ? x + MAX_BIT : x;
+  const u256 = isOdd ? x + MAX_BIT : x;
+  console.log("u256", u256);
+  const theSame = u256 === publicKeyBigint;
+  console.log("theSame", theSame);
+  return u256;
 }
 
 export async function u256ToPublicKey(u256: bigint): Promise<PublicKey> {
