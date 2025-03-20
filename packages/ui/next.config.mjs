1b051f4c24c35925868c97a283b1819777d06ab6
DFST <mk@dfst.io>
2025-03-20 23:39:07 +0300

    vercel 6

--- packages/ui/next.config.mjs
+++ packages/ui/next.config.mjs
@@ -1,13 +1,13 @@
 import path from "node:path";
 import { withLogtail } from "@logtail/next";
+import transpileModules from "next-transpile-modules";
 import { fileURLToPath } from "url";
-// const { default: bundleAnalyzer } = await import('@next/bundle-analyzer');
 
-// const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
+// Set up next-transpile-modules for @dex-example/lib
+const withTM = transpileModules(["@dex-example/lib"]);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -33,19 +33,20 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-
   webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
-    // If process.cwd() ends with "packages/ui", assume repository root is one level up.
+    // Optional: Add an alias if needed. If your workspace is structured so that the files are not in node_modules,
+    // this alias helps resolve the location. Adjust as necessary.
     const repoRoot = process.cwd().endsWith("packages/ui")
       ? path.join(process.cwd(), "..")
       : process.cwd();
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@dex-example/lib': path.join(repoRoot, "packages", "lib"),
-      'o1js': false,
+      "@dex-example/lib": path.join(repoRoot, "packages", "lib"),
+      // Optionally disable large libraries that you don't need:
+      "o1js": false,
     };
     return config;
   },
 };
 
-export default withLogtail(nextConfig);
+export default withTM(withLogtail(nextConfig));
