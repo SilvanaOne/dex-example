18ca36695a0339f4fda1533f04fe0af29580d5e9
DFST <mk@dfst.io>
2025-03-20 22:37:06 +0300

    vercel

--- packages/ui/src/lib/dex/public-key.ts
+++ packages/ui/src/lib/dex/public-key.ts
@@ -1,12 +1,15 @@
 "use server";
 
+import Client from "mina-signer";
 import { PublicKey } from "o1js";
 
 const MAX_BIT: bigint = 2n ** 255n;
+const client = new Client({
+  network: "testnet",
+});
 
-export async function publicKeyToU256(
-  publicKey: PublicKey | string
-): Promise<bigint> {
+export async function publicKeyToU256(publicKey: string): Promise<bigint> {
+  client.publicKeyToRaw;
   const publicKeyInternal: PublicKey =
     typeof publicKey === "string" ? PublicKey.fromBase58(publicKey) : publicKey;
   const x = publicKeyInternal.x.toBigInt();
