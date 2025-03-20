589eb453c40d635c59f17e17e59cbb69b38d3de8
DFST <mk@dfst.io>
2025-03-20 23:12:44 +0300

    remove o1js

--- packages/ui/src/lib/dex/public-key.ts
+++ packages/ui/src/lib/dex/public-key.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import Client from "mina-signer";
-import { PublicKey } from "o1js";
 
 const MAX_BIT: bigint = 2n ** 255n;
 const client = new Client({
@@ -10,24 +9,22 @@ const client = new Client({
 
 export async function publicKeyToU256(publicKey: string): Promise<bigint> {
   const raw = client.publicKeyToRaw(publicKey);
-  const publicKeyBigint = BigInt("0x" + raw);
-  console.log("publicKeyBigint", publicKeyBigint);
-  const publicKeyInternal: PublicKey =
-    typeof publicKey === "string" ? PublicKey.fromBase58(publicKey) : publicKey;
-  const x = publicKeyInternal.x.toBigInt();
-  const isOdd = publicKeyInternal.isOdd.toBoolean();
-  const u256 = isOdd ? x + MAX_BIT : x;
-  console.log("u256", u256);
-  const theSame = u256 === publicKeyBigint;
-  console.log("theSame", theSame);
-  return u256;
-}
+  const swappedRaw =
+    raw
+      .match(/.{1,2}/g)
+      ?.reverse()
+      .join("") || "";
+  const u256 = BigInt("0x" + swappedRaw);
+  // console.log("publicKeyBigint", publicKeyBigint);
+  // const publicKeyInternal: PublicKey =
+  //   typeof publicKey === "string" ? PublicKey.fromBase58(publicKey) : publicKey;
+  // const x = publicKeyInternal.x.toBigInt();
+  // const isOdd = publicKeyInternal.isOdd.toBoolean();
+  // const u256 = isOdd ? x + MAX_BIT : x;
+  // console.log("publicKeyBigint", publicKeyBigint);
+  // console.log("u256", u256);
+  // const theSame = u256 === publicKeyBigint;
+  // console.log("theSame", theSame);
 
-export async function u256ToPublicKey(u256: bigint): Promise<PublicKey> {
-  const isOdd = u256 >= MAX_BIT;
-  const x: bigint = isOdd ? u256 - MAX_BIT : u256;
-  return PublicKey.from({
-    x,
-    isOdd,
-  });
+  return u256;
 }
