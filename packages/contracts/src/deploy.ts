38ae5f1e92a002b86166afff1d4fe9d40c8dbbb6
DFST <mk@dfst.io>
2025-03-27 00:51:08 +0300

    build

--- packages/contracts/src/deploy.ts
+++ packages/contracts/src/deploy.ts
@@ -76,7 +76,7 @@ export async function deployMinaContract(params: {
   const tx = await Mina.transaction(
     {
       sender: admin,
-      fee: 700_000_000,
+      fee: 100_000_000,
       memo: `Deploy Silvana DEX Contract`,
     },
     async () => {
